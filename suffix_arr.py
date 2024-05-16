5ba50ed161db4709831bddf080b25b74620153a1
Manish Sampath <manishsampath@ManishsPCurrent.lan>
2024-05-15 22:11:39 -0700

    starting suffix array implementation

---
+++ suffix_arr.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class SA:
+    def __init__(self, db):
+        self.db = db
+        self.suffix_arr = []
+
+    def sort(self):
+        self.suffix_arr = sorted([])
+    def search(self, word):
+        low = 0
+        high = len(self.db) - 1
+        index = low + high // 2
+        while low <= high:
+            str = "lmaooooo"
+
+
+        return self.suffix_arr[index]
+
+    def 
+
+
+
+
