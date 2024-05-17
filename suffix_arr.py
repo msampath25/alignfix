5f8f644daa32952f0b5c141cbef7c996bb90fb85
manishsampath <m25sampath@gmail.com>
2024-05-17 11:10:38 -0700

    Working implementation of pattern matching with suffix arrays

--- suffix_arr.py
+++ suffix_arr.py
@@ -3,21 +3,77 @@ import numpy as np
 class SA:
     def __init__(self, db):
         self.db = db
-        self.suffix_arr = []
+        self.suffix_arr = self.populate(self)
 
-    def sort(self):
-        self.suffix_arr = sorted([])
-    def search(self, word):
+    def populate(self):
+        suffixes = np.zeros(len(self.db))
+        indices = np.zeros(len(self.db))
+
+        for i in range(len(self.db)):
+            suffixes[i] = self.db[0+i:]
+
+        sorted_suffixes = np.sort(suffixes)
+        for i in range(len(sorted_suffixes)):
+            indices[i] = len(self.db) - len(sorted_suffixes[i])
+
+        return indices
+
+    def __searchFirstIndex__(self, word):
         low = 0
         high = len(self.db) - 1
-        index = low + high // 2
+        mid = -1
+        first = -42
         while low <= high:
-            str = "lmaooooo"
+            mid = (low + high) // 2
+            if self.db[mid:mid + len(word)] == word:
+                low = mid
+                mid -= 1
+                while self.db[mid:mid + len(word)] == word:
+                    low = mid
+                    mid -= 1
+                first = low
+                break
+            elif self.db[mid:mid + len(word)] < word:
+                low = mid + 1
+            else:
+                high = mid - 1
 
+        if word == self.db[first:first + len(word)]:
+            return first
+        else:
+            return -69
 
-        return self.suffix_arr[index]
+    def __searchLastIndex__(self, first, word):
+        low = first + 1
+        high = len(self.db) - 1
+        mid = -1
+        last = -42
+        while low <= high:
+            mid = (low + high) // 2
+            if self.db[mid:mid + len(word)] == word:
+                high = mid
+                mid += 1
+                while self.db[mid:] == word:
+                    high = mid
+                    mid += 1
+                last = high
+                break
+            elif self.db[mid:] < word:
+                low = mid + 1
+            else:
+                high = mid - 1
+        if word == self.db[first:]:
+            return last
+        else:
+            return -69
 
-    def
+    def Seeds(self, word):
+        self.db += "$"
+        first_seed = self.__searchFirstIndex__(self, word)
+        last_seed = self.__searchLastIndex__(self, word)
+
+        return first_seed, last_seed
+
 
 
 
