d4a7f0a1ba87fc28da112597d03d123c892a87f0
JasonChiu001 <jmchiu@ucsd.edu>
2024-05-18 15:12:34 -0700

    Update Test_cases.py

--- Test_cases.py
+++ Test_cases.py
@@ -5,40 +5,40 @@ def test_suffix_array():
     assert list(sa.suffix_arr) == [0]
 
     sa = SA("a")
-    assert list(sa.suffix_arr) == [0]
+    assert list(sa.suffix_arr) == [1, 0]
 
     sa = SA("aaaaa")
-    assert list(sa.suffix_arr) == [0, 1, 2, 3, 4]
+    assert list(sa.suffix_arr) == [5, 4, 3, 2, 1, 0]
 
     sa = SA("abcde")
-    assert list(sa.suffix_arr) == [0, 1, 2, 3, 4]
+    assert list(sa.suffix_arr) == [5, 0, 1, 2, 3, 4]
 
     sa = SA("banana")
-    assert list(sa.suffix_arr) == [5, 3, 1, 0, 4, 2]
+    assert list(sa.suffix_arr) == [6, 5, 3, 1, 0, 4, 2]
 
     sa = SA("abracadabra")
-    assert list(sa.suffix_arr) == [10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2]
+    assert list(sa.suffix_arr) == [11, 10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2]
 
     sa = SA("mississippi")
-    assert list(sa.suffix_arr) == [10, 7, 4, 1, 0, 9, 8, 6, 3, 5, 2]
+    assert list(sa.suffix_arr) == [11, 10, 7, 4, 1, 0, 9, 8, 6, 3, 5, 2]
 
     sa = SA("hello, world!")
-    assert list(sa.suffix_arr) == [12, 11, 10, 0, 6, 8, 3, 5, 1, 7, 9, 4, 2]
+    assert list(sa.suffix_arr) == [6, 12, 13, 5, 11, 1, 0, 10, 2, 3, 4, 8, 9, 7]
 
     sa = SA("abc123def")
-    assert list(sa.suffix_arr) == [0, 1, 2, 6, 7, 8, 3, 4, 5]
+    assert list(sa.suffix_arr) == [9, 3, 4, 5, 0, 1, 2, 6, 7, 8]
 
     sa = SA("thequickbrownfoxjumpsoverthelazydog")
     assert list(sa.suffix_arr) == [
-        32, 20, 12, 24, 34, 10, 28, 22, 14, 26, 18, 30, 8, 16, 4, 6, 2, 0,
-        31, 19, 11, 23, 33, 9, 27, 21, 13, 25, 17, 29, 7, 15, 3, 5, 1
+        35, 29, 8, 6, 32, 27, 2, 23, 13, 34, 26, 1, 5, 16, 7, 28, 18, 12, 33, 21, 
+        10, 14, 19, 3, 9, 24, 20, 25, 0, 4, 17, 22, 11, 15, 31, 30
     ]
 
     sa = SA("hello\nworld")
-    assert list(sa.suffix_arr) == [10, 5, 0, 7, 9, 6, 1, 8, 2, 3, 4]
+    assert list(sa.suffix_arr) == [10, 9, 1, 0, 8, 2, 3, 6, 4, 7, 5]
 
     sa = SA("hello\tworld")
-    assert list(sa.suffix_arr) == [10, 5, 0, 7, 9, 6, 1, 8, 2, 3, 4]
+    assert list(sa.suffix_arr) == [10, 9, 1, 0, 8, 2, 3, 6, 4, 7, 5]
 
     print("All test cases passed!")
 
