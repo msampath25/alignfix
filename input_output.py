3e43b32b4bf95d6da1d6ad0e23ce0d25c72e1ff4
JasonChiu001 <jmchiu@ucsd.edu>
2024-05-20 12:53:31 -0700

    Template

--- input_output.py
+++ input_output.py
@@ -2,3 +2,7 @@
 #the they should be in this order: .fa and then .fq | we are going to be dealing with single end reads for this project
 #the output will be a .sam file
 
+input1 = open("input_fasta.fa", 'r').readlines()
+input2 = open("input_fastq.fq", 'r').readlines()
+
+output = open("output_sam.sam", 'w')
