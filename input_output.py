2ca8b542de043712bdf634be6c6afa1a1285ff15
SiddharthKaipa <101858219+SiddharthKaipa@users.noreply.github.com>
2024-05-20 12:50:31 -0700

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -1,1 +1,4 @@
+#deals with the input and output filetypes
+#the they should be in this order: .fa and then .fq | we are going to be dealing with single end reads for this project
+#the output will be a .sam file
 
