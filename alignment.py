fb02a10d548e9e1843d9baed3ce9f5cc094d9600
manishsampath <m25sampath@gmail.com>
2024-05-23 18:02:20 -0700

    Rough draft of the alignment.py is complete.

--- alignment.py
+++ alignment.py
@@ -1,17 +1,5 @@
 import numpy as np
 
-"""
-Goals: Create some sort of alignment given a predetermined l-mer match 
-
-Definitions: 
-    - We need to define some sort of metric for how large our matrix is gonna be. For a string of size 150, maybe
-      we only want an alignment that's size 200. 
-    - We need to define some sort of scoring parameter for our alignment. Do some research on how BLAST does it
-    - Do we want to implement affine gap penalties for our alignment? -> I think we should!
-    - Do we want to make a banded alignment? -> This will be rather difficult, although we could just do it without
-      making memory considerations
-    
-"""
 class Alignment(object):
 
     def __init__(self, db_truncated, query, seed, l, r):
@@ -25,6 +13,12 @@ class Alignment(object):
             the whole databse in memory
         query : string
             the read that we will align to the database
+        seed: integer
+            the position of the seed in the database
+        l : integer
+            the length of the seed
+        r: integer
+            the length of the alignment window
         Returns
         -------
         void
@@ -42,25 +36,7 @@ class Alignment(object):
         for i in range(len(self.query) - self.l + 1):
             if self.query[i:i + self.l] == self.db[self.seed: self.seed+self.l]:
                 return i, i + self.l
-
-
-    def __bottomAlignment__(self):
-        n = self.db[self.seed:]
-        m = self.query[self.lower_alignment:]
-        dp = np.zeros([len(m) + 1, len(n) + 1])
-
-        for i in range(1 , len(m) + 1):
-            dp[i][0] = dp[i-1][0] - 1
-
-        for j in range(1, len(n) + 1):
-            dp[0][j] = dp[0][j-1] -1
-
-        #I have to define a scoring function
-        for i in range(1, len(n) + 1):
-            for j in range(1, len(n) + 1):
-                dp[i][j] = 1
-
-    def output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty):
+    def __output_alignment__(self, lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty):
         if i == 0 and j == 0:
             return s_mod, t_mod
         if curr_graph is lower:
@@ -68,27 +44,26 @@ class Alignment(object):
                 curr_graph = middle
             s_mod = s[i - 1] + s_mod
             t_mod = '-' + t_mod
-            return output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i - 1, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
+            return self.output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i - 1, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
         elif curr_graph is upper:
             if j > 0 and upper[i][j] == middle[i][j - 1] - gap_opening_penalty:
                 curr_graph = middle
             s_mod = '-' + s_mod
             t_mod = t[j - 1] + t_mod
-            return output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j - 1, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
+            return self.output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j - 1, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
         else:
             if middle[i][j] == lower[i][j]:
                 curr_graph = lower
-                return output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
+                return self.output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
             elif middle[i][j] == upper[i][j]:
                 curr_graph = upper
-                return output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
+                return self.output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i, j, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
             else:
                 s_mod = s[i - 1] + s_mod
                 t_mod = t[j - 1] + t_mod
-                return output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i - 1, j - 1, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
+                return self.output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, i - 1, j - 1, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
 
-    # Inputted values must be positive, even for the penalties
-    def affine_alignment(match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty, s, t):
+    def __affine_alignment__(self, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty, s, t):
         sl = len(s)
         tl = len(t)
         lower = np.full((sl + 1, tl + 1), 0, dtype=int)
@@ -118,11 +93,28 @@ class Alignment(object):
         s_mod = ""
         t_mod = ""
         curr_graph = middle
-        s_mod, t_mod = output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, sl, tl, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
+        s_mod, t_mod = self.output_alignment(lower, middle, upper, curr_graph, s_mod, t_mod, s, t, sl, tl, match_reward, mismatch_penalty, gap_opening_penalty, gap_extension_penalty)
         
         return middle[sl][tl], s_mod, t_mod
-    
-    def align(self):
+
+    def __bottomAlignment__(self):
+        m = self.query[self.lower_alignment:]
+        n = self.db[self.seed + 1:]
+
+        return self.affine_alignment(2, 3, 5, 2, m, n)
+
+    def __topAlignment__(self):
+        #might be off by one here potentially
+        m = self.query[:self.upper_alignment]
+        n = self.db[:self.seed]
+
+        rev_m = reversed(m)
+        rev_n = reversed(n)
+        return self.affine_alignment(2, 3, 5, 2, rev_m, rev_n)
+    def Align(self):
+        top = self.__topAlignment__()
+        bottom = self.__bottomAlignment__()
+        return top, bottom
 
 
 
