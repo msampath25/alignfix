451f392b71199b08277e41d8f187d067775c3683
JasonChiu001 <jmchiu@ucsd.edu>
2024-05-27 17:21:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ setup(
         'numpy',
         'pyfaidx'
     ],
-    author='Jason Chiu, Author 2, Author 3',
-    author_email='jmchiu@ucsd.edu, author2@example.com, author3@example.com',
+    author='Jason Chiu, Manish Sampath, Siddharth Kaipa',
+    author_email='jmchiu@ucsd.edu, msampath@ucsd.edu, skaipa@ucsd.edu',
     description='A tool for aligning short Illumina reads to a genome based on a seed and extend method.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/alignfix',
+    url='https://github.com/msampath25/alignfix',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
